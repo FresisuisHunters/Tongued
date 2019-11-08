fd6d1ffbaba6d442f439e0d427e75fbad19e30b4
Jormii <lopeznatalj@gmail.com>
2019-11-08 01:03:16 +0100

    Interfaz ligeramente mejorada. Queda crear partida multijugador

--- Camaleones/Assets/Scripts/Server/OnlineLobbyManager.cs
+++ Camaleones/Assets/Scripts/Server/OnlineLobbyManager.cs
@@ -6,22 +6,56 @@ using UnityEngine.UI;
 
 public class OnlineLobbyManager : MonoBehaviourPunCallbacks {
 
+    #region Constant Fields
+
     private const string CONNECTION_STATUS_MESSAGE = "Connection status:\n";
 
+    #endregion
+
+    #region Public Fields
+
     public TextMeshProUGUI connectionStatusText;
+
+    // Ask Username Panel
+    public GameObject askUsernamePanel;
     public TMP_InputField usernameInputField;
-    public GameObject buttonsPanel;
-    public Button createRoomButton;
-    public Button joinRandomRoomButton;
-    public Button listRoomsButton;
+
+    // Lobby Menu Panel
+    public GameObject lobbyMenuPanel;
+
+    // Create Room Panel
+    public GameObject createRoomPanel;
+    public TMP_InputField roomNameInputField;
+    public TMP_Dropdown roomSizeDropdown;
+
+    // Room Panel
+    public GameObject roomPanel;
+    public TextMeshProUGUI playerListText;
+
+    #endregion
+
+    #region Private Fields
+
+    [SerializeField] private string playerUsername;
+    private GameObject activePanel;
+
+    #endregion
+
+    #region Unity Callbacks
 
     private void Awake () {
         PhotonNetwork.AutomaticallySyncScene = true;
+
+        activePanel = askUsernamePanel;
     }
 
-    private void Start () {
-        ConnectToPhotonServer ();
+    private void Update () {
+        connectionStatusText.text = CONNECTION_STATUS_MESSAGE + PhotonNetwork.NetworkClientState;
     }
+
+    #endregion
+
+    #region Private Methods
 
     private void ConnectToPhotonServer () {
         if (PhotonNetwork.IsConnected) {
@@ -33,14 +67,30 @@ public class OnlineLobbyManager : MonoBehaviourPunCallbacks {
         PhotonNetwork.ConnectUsingSettings ();
     }
 
-    private void Update () {
-        connectionStatusText.text = CONNECTION_STATUS_MESSAGE + PhotonNetwork.NetworkClientState;
+    private void UpdatePlayersList () {
+        Player[] playersInRoom = PhotonNetwork.PlayerList;
+
+        playerListText.text = "";
+        for (int i = 0; i < playersInRoom.Length; ++i) {
+            Player player = playersInRoom[i];
+
+            playerListText.text += player.ToString () + "\n";
+        }
     }
+
+    private void SwitchPanels (GameObject newPanel) {
+        activePanel.SetActive (false);
+        newPanel.SetActive (true);
+        activePanel = newPanel;
+    }
+
+    #endregion
 
     #region Photon Callbacks
 
     public override void OnConnectedToMaster () {
         Debug.Log ("OnConnectedToMaster");
+        SwitchPanels (lobbyMenuPanel);
     }
 
     public override void OnLeftLobby () {
@@ -64,6 +114,9 @@ public class OnlineLobbyManager : MonoBehaviourPunCallbacks {
 
     public override void OnJoinedRoom () {
         Debug.Log ("OnJoinedRoom");
+
+        SwitchPanels (roomPanel);
+        UpdatePlayersList ();
     }
 
     public override void OnLeftRoom () {
@@ -73,6 +126,8 @@ public class OnlineLobbyManager : MonoBehaviourPunCallbacks {
     public override void OnPlayerEnteredRoom (Player newPlayer) {
         Debug.Log ("OnPlayerEnteredRoom");
         Debug.Log (newPlayer.NickName);
+
+        UpdatePlayersList ();
     }
 
     public override void OnPlayerLeftRoom (Player otherPlayer) {
@@ -84,16 +139,35 @@ public class OnlineLobbyManager : MonoBehaviourPunCallbacks {
 
     #region UI Callbacks
 
-    public void OnCreateRoomButtonClicked () {
+    public void OnConnectToServerButtonPressed () {
+        // TODO: Crear un Player de Photon con esta informacion
+        playerUsername = usernameInputField.text;
 
+        ConnectToPhotonServer ();
+    }
+
+    public void OnCreateRoomButtonClicked () {
+        SwitchPanels (createRoomPanel);
     }
 
     public void OnJoinRandomRoomButtonClicked () {
-
+        PhotonNetwork.JoinRandomRoom ();
     }
 
     public void OnListRoomsButtonClicked () {
+        Debug.LogWarning ("Queda implementar listar las salas existentes");
+    }
 
+    public void OnCreateButtonClicked () {
+        string roomName = roomNameInputField.text;
+
+        int dropdownSelectedItemIndex = roomSizeDropdown.value;
+        string roomSizeString = roomSizeDropdown.options[dropdownSelectedItemIndex].text;
+        byte roomSize = byte.Parse (roomSizeString);
+
+        RoomOptions roomOptions = new RoomOptions ();
+        roomOptions.MaxPlayers = roomSize;
+        PhotonNetwork.CreateRoom (roomName, roomOptions);
     }
 
     #endregion
