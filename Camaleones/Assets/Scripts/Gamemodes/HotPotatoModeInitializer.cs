ad9b09ccbf00134a3af1b417c8d1787e2ea137ae
Aitor Iribar <aitor.iribar@gmail.com>
2019-11-16 09:32:19 +0100

    Creado HotPotatoModeInitializer

---
+++ Camaleones/Assets/Scripts/Gamemodes/HotPotatoModeInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Componente que inicializa el modo de juego de patata caliente. 
+/// No debería formar parte de la escena. Al cargar una partida de este modo, debería instanciarse este prefab para inicializar el mapa con este modo.
+/// </summary>
+public class HotPotatoModeInitializer : MonoBehaviour
+{
+    public TransferableItem snitch;
+
+    private void Awake()
+    {
+        GameObject spawnPoint = GameObject.FindGameObjectWithTag("SnitchSpawnPoint");
+        if (!spawnPoint)
+        {
+            Debug.LogError("There is no SnitchSpawnPoint in the scene.");
+        }
+
+        Instantiate(snitch, spawnPoint.transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
+    }
+}
