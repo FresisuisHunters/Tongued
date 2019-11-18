e6818a3ee3f83474185b32441e0ae2b8fe4a39c0
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2019-11-18 13:29:57 +0100

    Hecha clase online de TransferableItem pero sin rellenar por dudas

---
+++ Camaleones/Assets/Scripts/Online/OnlineTransferableItem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(PhotonView))]
+public class OnlineTransferableItem : TransferableItem, IPunObservable, IPunInstantiateMagicCallback
+{
+    private PhotonView photonView;
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        
+    }
+
+    public void OnPhotonInstantiate(PhotonMessageInfo info)
+    {
+        
+    }
+
+
+}
