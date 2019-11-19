86f63ad81a6271f2a429b6b1c70c44d75a42c0ac
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2019-11-19 22:45:02 +0100

    Un poco más de código de hot potato

--- Camaleones/Assets/Scripts/Gamemodes/HotPotatoHandler.cs
+++ Camaleones/Assets/Scripts/Gamemodes/HotPotatoHandler.cs
@@ -15,13 +15,21 @@ public class HotPotatoHandler : MonoBehaviour
     [Tooltip("Tiempo inicial que necesita tener un jugador la snitch para ganar. Se ira reduciendo durante la ronda")]
     [SerializeField] private float baseSnitchPossesionTimer;
     [Tooltip("Curva que define como se reduce el tiempo necesario de posesión de la snitch para cada ronda")]
-    [SerializeField] private AnimationCurve possesionTimeCurve;
+    [SerializeField] private AnimationCurve posessionTimeCurve;
     [Tooltip("Prefab de la snitch")]
     [SerializeField] private GameObject snitch;
     #endregion
 
     #region Private Variables
+    //Instante en el que empieza la ronda
+    private float roundStartTime;
+    //Instante en el que empieza el timer de la última posesión de la snitch
     private float timerStartTime;
+    //tiempo final para el timer en un momento concreto. Se reevalua mirando posessionTimeCurve
+    private float currentEndTime;
+    //Timer que se muestra en pantalla
+    private int timerCurrentTime;
+    private bool hasStarted;
     #endregion
 
     /// <summary>
@@ -36,7 +44,9 @@ public class HotPotatoHandler : MonoBehaviour
         }
 
         GameObject snitchReference = Instantiate(snitch, spawnPoint.transform.position, Quaternion.identity);
-        snitchReference.getComponent<TransferableItem>().hotPotatoHandler = this;
+        snitchReference.GetComponent<TransferableItem>().hotPotatoHandler = this;
+        hasStarted = false;
+        roundStartTime = Time.time;
     }
 
     /// <summary>
@@ -44,7 +54,7 @@ public class HotPotatoHandler : MonoBehaviour
     /// </summary>
     public void NotifyTransfer()
     {
-
+        timerStartTime = Time.time;
     }
 
     // Start is called before the first frame update
@@ -56,6 +66,17 @@ public class HotPotatoHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void UpdateTimer()
+    {
+
+    }
+
+    private void RestartTimer()
+    {
+        timerStartTime = Time.time;
+        currentEndTime = posessionTimeCurve.Evaluate(Time.time - roundStartTime);
     }
 }
