082cee9b8645b56854d04358418a0217740d8978
Jorge Lopez Natal <lopeznatalj@gmail.com>
2019-11-19 15:59:46 +0100

    Corregido el codigo de TouchHookInput.cs

--- Camaleones/Assets/Scripts/Hook Stuff/TouchHookInput.cs
+++ Camaleones/Assets/Scripts/Hook Stuff/TouchHookInput.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [RequireComponent (typeof (HookThrower))]
 public class TouchHookInput : MonoBehaviour {
 
+    private const int NO_FINGER = -1;
+
     private HookThrower hookThrower;
     private Vector3 pressedPosition;
     private bool isPressingScreen;
@@ -10,52 +12,55 @@ public class TouchHookInput : MonoBehaviour {
 
     private void Awake () {
         hookThrower = GetComponent<HookThrower> ();
-        fingerId = -1;
+        fingerId = NO_FINGER;
     }
 
     private void Update () {
+        if (Input.touchCount == 0) {
+            return;
+        }
+
+        bool fingerLifted = !HandleScreenTouches();
+        if (fingerLifted) {
+            OnFingerLifted ();
+        } else if (hookThrower.HookIsOut) {
+            hookThrower.Retract (Time.deltaTime);
+        }
+    }
+
+    private bool HandleScreenTouches() {
+        // TODO: Es un poco confuso el fingerLifted
         bool fingerLifted = true;
         foreach (Touch touchInput in Input.touches) {
-            int touchFingerId = touchInput.fingerId;
-            if (fingerId == -1) {
+            if (fingerId == NO_FINGER) {
                 OnScreenPressed (touchInput);
             }
 
-            if (touchFingerId != fingerId) {
+            if (touchInput.fingerId != fingerId) {
                 continue;
             }
 
             fingerLifted = false;
         }
 
-        if (fingerLifted) {
-            OnFingerLifted ();
-        } else {
-            HandleInput ();
-        }
+        return !fingerLifted;
     }
 
     private void OnScreenPressed (Touch touchInput) {
         isPressingScreen = true;
         fingerId = touchInput.fingerId;
         pressedPosition = Camera.main.ScreenToWorldPoint (touchInput.position);
+
+        if (!hookThrower.HookIsOut) {
+            hookThrower.ThrowHook (pressedPosition);
+        }
     }
 
     private void OnFingerLifted () {
         isPressingScreen = false;
-        fingerId = -1;
+        fingerId = NO_FINGER;
 
         hookThrower.LetGo ();
-    }
-
-    private void HandleInput () {
-        if (!hookThrower.HookIsOut) {
-            hookThrower.ThrowHook (pressedPosition);
-        }
-
-        if (hookThrower.HookIsOut) {
-            hookThrower.Retract (Time.deltaTime);
-        }
     }
 
 }
