73e3690f469c9a7dd8a72676699ff0a92b0d6b05
Aitor Iribar <aitor.iribar@gmail.com>
2019-11-20 20:01:53 +0100

    Escena online con el nivel de prueba

--- Camaleones/Assets/Scripts/Online/ServerConstants.cs
+++ Camaleones/Assets/Scripts/Online/ServerConstants.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class ServerConstants {
 
     public const string GAME_VERSION = "b0.1";
-    public const string ONLINE_LEVEL = "Online_Level";
+    public const string ONLINE_LEVEL = "sce_tOnlinePlay";
 
     #region Game Mode Constants
 
