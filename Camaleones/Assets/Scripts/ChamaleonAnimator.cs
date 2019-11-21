8eb6866cf54ffc968496c9cdb1acbc5b21d9340b
Aitor Iribar <aitor.iribar@gmail.com>
2019-11-21 10:19:56 +0100

    AnimationController de la cabeza hecho

---
+++ Camaleones/Assets/Scripts/ChamaleonAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChamaleonAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator headAnimator;
+    [SerializeField] private Animator bodyAnimator;
+
+    private static int IDLE_STATE = Animator.StringToHash("Idle");
+    private static int GROUNDED_STATE = Animator.StringToHash("Grounded");
+    private static int THROW_STATE = Animator.StringToHash("Throw");
+    private static int HANG_STATE = Animator.StringToHash("Hang");
+    private static int FALL_STATE = Animator.StringToHash("Fall");
+    private static int POS_REACT_STATE = Animator.StringToHash("PosReact");
+    private static int NEG_REACT_STATE = Animator.StringToHash("NegReact");
+
+    private void Start()
+    {
+        headAnimator.Play(HANG_STATE, 0, 0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            headAnimator.Play(POS_REACT_STATE, 1, 0);
+        }
+    }
+}
