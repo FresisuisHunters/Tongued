91ba519ae4986999e736a8555eb221378d7ddc9e
Jormii <lopeznatalj@gmail.com>
2019-11-21 19:55:11 +0100

    Input implementado. Añadir feedback visual

--- Camaleones/Assets/Scripts/Hook Stuff/TouchHookInput.cs
+++ Camaleones/Assets/Scripts/Hook Stuff/TouchHookInput.cs
@@ -5,13 +5,17 @@ using Photon.Pun;
 [RequireComponent (typeof (HookThrower))]
 public class TouchHookInput : MonoBehaviour {
 
+    private const float DISTANCE_ACTIVATE_RECTRACT_SCREEN_SPACE = 30f;
+
     private HookThrower hookThrower;
+    private Vector2 positionTouchedScreenCoordinates;
     private bool touchingScreen;
-    private Vector3 positionTouchedScreenCoordinates;
+    private bool retract;
 
     private void Awake() {
         hookThrower = GetComponent<HookThrower>();
         touchingScreen = false;
+        retract = false;
     }
 
     private void Start() {
@@ -29,10 +33,11 @@ public class TouchHookInput : MonoBehaviour {
 
         inputEventReceiver.AddListener(EventTriggerType.PointerDown, (data) => OnPointerDown(data as PointerEventData));
         inputEventReceiver.AddListener(EventTriggerType.PointerUp, (data) => OnPointerUp());
+        inputEventReceiver.AddListener(EventTriggerType.Drag, (data) => OnPointerDrag(data as PointerEventData));
     }
 
     private void Update() {
-        if (hookThrower.HookIsOut && touchingScreen) {
+        if (retract) {
             hookThrower.Retract(Time.deltaTime);
         }
     }
@@ -45,14 +50,24 @@ public class TouchHookInput : MonoBehaviour {
         touchingScreen = true;
         positionTouchedScreenCoordinates = eventData.position;
         if (!hookThrower.HookIsOut) {
-            Vector3 positionTouchedWorldCoordinates = eventData.pressEventCamera.ScreenToWorldPoint(positionTouchedScreenCoordinates);
+            Vector2 positionTouchedWorldCoordinates = eventData.pressEventCamera.ScreenToWorldPoint(positionTouchedScreenCoordinates);
             hookThrower.ThrowHook(positionTouchedWorldCoordinates);
         }
     }
 
     private void OnPointerUp() {
         touchingScreen = false;
+        retract = false;
         hookThrower.LetGo();
+    }
+
+    private void OnPointerDrag(PointerEventData eventData) {
+        Vector2 screenPosition = eventData.position;
+        float distanceToOriginalTouch = Vector2.Distance(positionTouchedScreenCoordinates, screenPosition);
+
+        Debug.Log(distanceToOriginalTouch);
+
+        retract = distanceToOriginalTouch >= DISTANCE_ACTIVATE_RECTRACT_SCREEN_SPACE;
     }
 
 }
