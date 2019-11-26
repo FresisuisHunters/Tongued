9b2ab134711e32304e39b0cee7669c985d47df16
Aitor Iribar <aitor.iribar@gmail.com>
2019-11-26 12:42:08 +0100

    Se comunica visualmente que la lengua se va a soltar

--- Camaleones/Assets/Scripts/Gameplay/HookedReactions/AttachedStruggler.cs
+++ Camaleones/Assets/Scripts/Gameplay/HookedReactions/AttachedStruggler.cs
@@ -7,8 +7,10 @@ using UnityEngine;
 [RequireComponent(typeof(Hook))]
 public class AttachedStruggler : MonoBehaviour
 {
-    private Hook hook;
+    public Color finalTongueTint;
 
+    private Hook hook;
+    private LineRenderer lineRenderer;
 
     public void DetachAfterCountdown(float countdownLength)
     {
@@ -18,19 +20,40 @@ public class AttachedStruggler : MonoBehaviour
 
     private IEnumerator _DisableHookAfterCountdown(float countdownLength)
     {
-        yield return new WaitForSeconds(countdownLength);
-        //TODO: Alguna animación aquí. Tintar su color hacia el rojo?
+        float t = 0;
+        Color white = Color.white;
+        Color lerpedColor;
+        do
+        {
+            lerpedColor = Color.Lerp(white, finalTongueTint, t / countdownLength);
+            lineRenderer.startColor = lerpedColor;
+            lineRenderer.endColor = lerpedColor;
+            t += Time.deltaTime;
+
+            yield return null;
+        } while (t < countdownLength);
+        
         hook.Disable();
+        ResetTint();
     }
 
     private void CancelCountdown()
     {
         StopAllCoroutines();
+        ResetTint();
+    }
+
+    private void ResetTint()
+    {
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
     }
 
     private void Awake()
     {
         hook = GetComponent<Hook>();
         hook.OnDisabled += CancelCountdown;
+
+        lineRenderer = GetComponentInChildren<LineRenderer>();
     }
 }
