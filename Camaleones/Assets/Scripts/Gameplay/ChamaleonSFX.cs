9feb4dfbd9feceb5f59dd37fea499240e219bbdd
Aitor Iribar <aitor.iribar@gmail.com>
2019-12-01 09:15:49 +0100

    Implementados SFX de agua

--- Camaleones/Assets/Scripts/Gameplay/ChamaleonSFX.cs
+++ Camaleones/Assets/Scripts/Gameplay/ChamaleonSFX.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Audio;
 using Hairibar.Audio.SFX;
 
 #pragma warning disable 649
 [RequireComponent(typeof(OneShotSFXPlayer), typeof(HookThrower), typeof(Rigidbody2D)), RequireComponent(typeof(RepeatAtFrequencySFXPlayer))]
 public class ChamaleonSFX : MonoBehaviour
 {
+    #region Inspector
     [Header("Retract")]
     [SerializeField] private float retractSFXMaxFrequency = 10;
     [SerializeField] private SFXClip retractSFX;
@@ -16,15 +16,29 @@ public class ChamaleonSFX : MonoBehaviour
     [SerializeField] private float minVelocityForWeeeSFX = 10;
     [SerializeField, Range(0, 1)] private float weeeSFXChance = 0.3f;
 
+    [Header("Water")]
+    [SerializeField] private SFXClip localSplashIn;
+    [SerializeField] private SFXClip remoteSplashIn;
+    [SerializeField] private SFXClip splashOut;
+    [SerializeField] private AudioMixerSnapshot underwaterSnapshot;
+    [SerializeField] private AudioMixerSnapshot normalGameplaySnapshot;
+    [SerializeField] private float snapshotTransitionLength;
+    [SerializeField] private float minVelocityForSplash = 5;
+
     [Header("Others")]
     [SerializeField] private SFXClip throwHookSFX;
     [SerializeField] private SFXClip disableHookSFX;
+    #endregion
 
+
+    #region References
     private new Rigidbody2D rigidbody;
     private HookThrower hookThrower;
     private OneShotSFXPlayer sfxPlayer;
     private RepeatAtFrequencySFXPlayer retractPlayer;
+    #endregion
 
+    bool isLocalPlayer;
     private float retractAmountThisFrame;
 
     private void LateUpdate()
@@ -45,7 +59,33 @@ public class ChamaleonSFX : MonoBehaviour
             sfxPlayer.RequestSFX(disableHookSFX);
         }
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Water") && Mathf.Abs(rigidbody.velocity.y) > minVelocityForSplash)
+        {
+            if (isLocalPlayer)
+            {
+                sfxPlayer.RequestSFX(localSplashIn);
+                underwaterSnapshot.TransitionTo(snapshotTransitionLength);
+            }
+            else
+            {
+                sfxPlayer.RequestSFX(remoteSplashIn);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Water"))
+        {
+            if (isLocalPlayer) normalGameplaySnapshot.TransitionTo(snapshotTransitionLength);
+            if (Mathf.Abs(rigidbody.velocity.y) > minVelocityForSplash) sfxPlayer.RequestSFX(splashOut);
+        }
+    }
+
+    #region Initialization
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
@@ -58,8 +98,11 @@ public class ChamaleonSFX : MonoBehaviour
 
     private void Start()
     {
+        isLocalPlayer = GetComponent<Photon.Pun.PhotonView>()?.IsMine ?? true;
+
         hookThrower.OnHookThrown += (Vector2 target) => sfxPlayer.RequestSFX(throwHookSFX);
         hookThrower.OnHookDisabled += OnHookDisabled;
         hookThrower.OnRetracted += (float amount) => retractAmountThisFrame = amount;
     }
+    #endregion
 }
