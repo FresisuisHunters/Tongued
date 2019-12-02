427a11525ad9c9c33984b26a91a7698f6b58e8a0
Aitor Iribar <aitor.iribar@gmail.com>
2019-12-02 11:09:48 +0100

    Puesto que se utilice Level3 para jugar online

--- Camaleones/Assets/Scripts/Online/ServerConstants.cs
+++ Camaleones/Assets/Scripts/Online/ServerConstants.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class ServerConstants {
 
     public const string GAME_VERSION = "b0.1";
-    public const string ONLINE_LEVEL = "sce_tAnimationDev";
+    public const string ONLINE_LEVEL = "sce_tLevel3";
 
     #region Game Mode Constants
 
