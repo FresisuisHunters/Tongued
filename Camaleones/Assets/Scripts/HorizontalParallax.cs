219e9ed4bc4b279c03a665f3d1f86bd4a978311a
Aitor Iribar <aitor.iribar@gmail.com>
2019-12-03 20:39:11 +0100

    Parallax horizontal bien

---
+++ Camaleones/Assets/Scripts/HorizontalParallax.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class HorizontalParallax : MonoBehaviour
+{
+    public float velocityFactor = 0.1f;
+
+    //private new Camera camera;
+    private Transform cameraTransform;
+    private float lastCameraPos;
+
+
+    private void Update()
+    {
+        float newCameraPos = cameraTransform.position.x;
+        float worldSpaceCameraDelta = newCameraPos - lastCameraPos;
+
+        transform.position += new Vector3(worldSpaceCameraDelta * velocityFactor, 0, 01);
+        lastCameraPos = newCameraPos;
+    }
+
+
+    private void Start()
+    {
+        cameraTransform = Camera.main.transform;
+    }
+}
---
+++ Camaleones/Assets/Scripts/Menus/AlphaThresholdForImageEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class AlphaThresholdForImageEvents : MonoBehaviour
+{
+    [SerializeField, Range(0, 1)] private float alphaThreshold = 0.1f;
+
+    private void Start()
+    {
+        Apply();
+    }
+
+    private void OnValidate()
+    {
+        Apply();
+    }
+
+    private void Apply()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = alphaThreshold;
+    }
+
+}
