ba9ee86b1e32658bf86deb718bfbc5a701bf70b4
Aitor Iribar <aitor.iribar@gmail.com>
2019-12-05 17:00:06 +0100

    Sala bonita

--- Camaleones/Assets/Scripts/Menus/RoomPlayerEntry.cs
+++ Camaleones/Assets/Scripts/Menus/RoomPlayerEntry.cs
@@ -4,81 +4,38 @@ using UnityEngine.UI;
 using Photon.Pun;
 
 #pragma warning disable 649
-public class RoomPlayerEntry : MonoBehaviour {
-
-    #region Private Fields
-
+public class RoomPlayerEntry : MonoBehaviour
+{
     [SerializeField] private TextMeshProUGUI playerNameText;
     [SerializeField] private Button playerReadyButton;
-    private RectTransform rectTransform;
-    private string playerName;
-    private bool buttonClicked;
+    [SerializeField] private Button playerWaitingButton;
 
-    #endregion
-
-    #region Unity Callbacks
-
-    private void Awake () {
-        rectTransform = GetComponent<RectTransform>();
-        buttonClicked = false;
-        playerReadyButton.onClick.AddListener (() => OnPlayerReadyButtonClicked ());
-    }
-
-    #endregion
-
-    #region Public Methods
-    public void ShowButton(bool show) {
-        playerReadyButton.gameObject.SetActive(show);
-    }
-
-    public override string ToString() {
-        return string.Format("ROOM_PLAYER_ENTRY: {0}, {1}", playerName, rectTransform.position);
-    }
-    #endregion
-
-    #region UI Callbacks
-
-    private void OnPlayerReadyButtonClicked () {
-        buttonClicked = !buttonClicked;
-        if (buttonClicked) {
-            Room.photonView.RPC("RPC_PlayerIsReady", RpcTarget.All, new object[] { playerName });
-        } else {
-           Room.photonView.RPC("RPC_PlayerNotReady", RpcTarget.All, new object[] { playerName });
-        }
-    }
-
-    #endregion
-
-    #region Properties
-
-    public Vector3 Position {
-        get => rectTransform.position;
-        set {
-            rectTransform.localPosition = value;
-        }
-    }
-
-    public bool Visible {
-        set => gameObject.SetActive (value);
-    }
-
-    public string PlayerName {
+    public string PlayerName
+    {
         get => playerNameText.text;
-        set {
-            playerName = value;
+        set
+        {
             playerNameText.text = value;
 
-            bool isLocalPlayer = PhotonNetwork.LocalPlayer.NickName.Equals(playerName);
-            gameObject.SetActive(isLocalPlayer);
+            bool isLocalPlayer = PhotonNetwork.LocalPlayer.NickName.Equals(value);
+            playerReadyButton.enabled = isLocalPlayer;
+            playerWaitingButton.enabled = isLocalPlayer;
         }
     }
 
-    public string Text {
-        set => playerNameText.text = value;
+    public bool IsReady
+    {
+        set
+        {
+            playerReadyButton.gameObject.SetActive(!value);
+            playerWaitingButton.gameObject.SetActive(value);
+        }
     }
 
-    public RoomScreen Room { get; set; }
+    public void OnReadyButtonPressed(bool isReady)
+    {
+        GetComponentInParent<RoomScreen>().SetLocalPlayerReady(isReady);
 
-    #endregion
-
+        IsReady = isReady;
+    }
 }
