ecfb5b21a16f3879ecb1615c044c6c18df0437a9
Aitor Iribar <aitor.iribar@gmail.com>
2019-12-07 10:51:52 +0100

    Hacer click en el logo de Fresisuis abre el portfolio

---
+++ Camaleones/Assets/Scripts/Menus/OpenLinkOnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class OpenLinkOnClick : MonoBehaviour
+{
+    public string url;
+
+    private void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OpenLink);
+    }
+
+    private void OpenLink()
+    {
+        Application.OpenURL(url);
+    }
+}
