fac9e7057000f7fabc4791918b56fd28974c7921
Aitor Iribar <aitor.iribar@gmail.com>
2019-12-07 21:13:16 +0100

    Retoeques de audio, y se abre el link al portfolio en el explorador en sí en vez del iframe

--- Camaleones/Assets/Scripts/Menus/OpenLinkOnClick.cs
+++ Camaleones/Assets/Scripts/Menus/OpenLinkOnClick.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,8 +14,16 @@ public class OpenLinkOnClick : MonoBehaviour
         GetComponent<Button>().onClick.AddListener(OpenLink);
     }
 
-    private void OpenLink()
+    public void OpenLink()
     {
+#if WEBGL
+        openWindow(Field.text);
+#else
         Application.OpenURL(url);
+#endif
     }
+
+
+    [DllImport("__Internal")]
+    private static extern void openWindow(string url);
 }
