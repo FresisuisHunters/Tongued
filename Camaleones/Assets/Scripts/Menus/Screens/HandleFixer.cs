7efa08034e580fe358ec2183a466ad5c0f32903d
César Romero Albertos <43405217+CesarRomeroAlbertos@users.noreply.github.com>
2019-12-07 14:07:02 +0100

    Scrollbar súper cuco

---
+++ Camaleones/Assets/Scripts/Menus/Screens/HandleFixer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Este script se usa para fijar el tamaño del handle de la scrollbar de los créditos a la fuerza y también para ajustar el tamaño de la lengua
+/// </summary>
+public class HandleFixer : MonoBehaviour
+{
+    private Scrollbar scrollbar;
+    Image image;
+    private void Awake()
+    {
+        scrollbar = GetComponent<Scrollbar>();
+        scrollbar.size = 0.25f;
+        image = GetComponent<Image>();
+    }
+    private void FixedUpdate()
+    {
+        scrollbar.size = 0.25f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        scrollbar.size = 0.25f;
+        image.fillAmount = Mathf.Lerp(0.18f, 1, 1-scrollbar.value);
+    }
+}
