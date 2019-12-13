b5c8d9b6dc33ec816c68f846311a73589c72429a
Aitor Iribar <aitor.iribar@gmail.com>
2019-12-13 20:03:33 +0100

    Al pulsar enter en AskUsernameScreen, se pulsa el botón de conectar

---
+++ Camaleones/Assets/InvokeOnClickWhenKeyPressed.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class InvokeOnClickWhenKeyPressed : MonoBehaviour
+{
+    public KeyCode key = KeyCode.Return;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            GetComponent<Button>().onClick.Invoke();
+        }
+    }
+}
